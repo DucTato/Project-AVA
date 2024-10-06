c385808ed56b6db40970177edee6e68b09615106
DucTato <65166409+DucTato@users.noreply.github.com>
2024-10-07 00:35:04 +0700

    InputSystem
    Migrated the old Input Manager system to the new Input System.
    Added Gamepad support

--- Assets/Scripts/HeliHandler.cs
+++ Assets/Scripts/HeliHandler.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HeliHandler : MonoBehaviour
 {
+    private GamepadControls gpControls;
     private Rigidbody rb;
     [SerializeField]
     private float responsiveness = 500f;
@@ -15,24 +17,44 @@ public class HeliHandler : MonoBehaviour
     private float maxThrust = 5f;
     [SerializeField]
     private Transform rotorTransform;
-    private float throttle;
+    private float throttle, throttleValueTrigger;
     private float roll, pitch, yaw;
+    private Vector2 stickValue;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        gpControls = new GamepadControls();
+
+        gpControls.Gameplay.ACmovement.performed += context => stickValue = context.ReadValue<Vector2>();
+        gpControls.Gameplay.ACmovement.canceled += context => stickValue = Vector2.zero;
+
+        gpControls.Gameplay.ACyaw.performed += context => yaw = context.ReadValue<float>();
+        gpControls.Gameplay.ACyaw.canceled += context => yaw = 0f;
+
+        gpControls.Gameplay.ACthrottle.performed += context => throttleValueTrigger = context.ReadValue<float>();
+        gpControls.Gameplay.ACthrottle.canceled += context => throttleValueTrigger = 0f;
+    }
+    private void OnEnable()
+    {
+        gpControls.Gameplay.Enable();
+    }
+    private void OnDisable()
+    {
+        gpControls.Gameplay.Disable();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        yaw = 0f;
+        throttle = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleInput();
-        rotorTransform.Rotate(maxThrust * throttle * rotorSpeedMult * Vector3.up);
+        rotorTransform.Rotate(maxThrust * throttle * rotorSpeedMult * Time.deltaTime * Vector3.up);
     }
     private void FixedUpdate()
     {
@@ -44,22 +66,30 @@ public class HeliHandler : MonoBehaviour
         rb.AddTorque(-roll * responsiveness * transform.forward);
         rb.AddTorque(yaw * responsiveness * transform.up);
     }
+    //private void HandleInput()      // Legacy input system
+    //{
+    //    // Setting rotational values from axis inputs
+    //    roll = Input.GetAxis("Roll");
+    //    pitch = Input.GetAxis("Pitch");
+    //    yaw = Input.GetAxis("Yaw");
+
+    //    // Handling throttle inputs
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        throttle += throttleIncrement;
+    //    }
+    //    else if (Input.GetKey(KeyCode.S))
+    //    {
+    //        throttle -= throttleIncrement;
+    //    }
+    //    throttle = Mathf.Clamp(throttle, 0f, 100f);
+    //}
+
     private void HandleInput()
     {
-        // Setting rotational values from axis inputs
-        roll = Input.GetAxis("Roll");
-        pitch = Input.GetAxis("Pitch");
-        yaw = Input.GetAxis("Yaw");
-
-        // Handling throttle inputs
-        if (Input.GetKey(KeyCode.W))
-        {
-            throttle += throttleIncrement;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            throttle -= throttleIncrement;
-        }
+        roll = stickValue.x;
+        pitch = stickValue.y;
+        throttle += (throttleIncrement * throttleValueTrigger);
         throttle = Mathf.Clamp(throttle, 0f, 100f);
     }
 }
--- Assets/Scripts/PlaneHandler.cs
+++ Assets/Scripts/PlaneHandler.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlaneHandler : MonoBehaviour
 {
+    private GamepadControls gpControls;
     private Rigidbody rb;
     [Header("Plane Stats")]
     [SerializeField]
@@ -17,9 +19,10 @@ public class PlaneHandler : MonoBehaviour
     private float responsiveness = 10f;
     [SerializeField]
     private float liftForce = 135f;
-    private float throttle;
+    private float throttle, throttleValueTrigger;
     private float roll, pitch, yaw;
-    
+    private Vector2 stickValue;
+
     // Taking the plane's mass into tweaking its responsiveness
     private float responsibilityModifier
     {
@@ -29,21 +32,43 @@ public class PlaneHandler : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        gpControls = new GamepadControls();
+        gpControls.Gameplay.ACmovement.performed += context => stickValue = context.ReadValue<Vector2>();
+        gpControls.Gameplay.ACmovement.canceled += context => stickValue = Vector2.zero;
+
+        gpControls.Gameplay.ACyaw.performed += context => yaw = context.ReadValue<float>();
+        gpControls.Gameplay.ACyaw.canceled += context => yaw = 0f;
+
+        gpControls.Gameplay.ACthrottle.performed += context => throttleValueTrigger = context.ReadValue<float>();
+        gpControls.Gameplay.ACthrottle.canceled += context => throttleValueTrigger = 0f;
+
+    }
+
+    private void OnEnable()
+    {
+        gpControls.Gameplay.Enable();
+    }
+    private void OnDisable()
+    {
+        gpControls.Gameplay.Disable();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        yaw = 0f;
+        throttle = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleInput();
+      
     }
     private void FixedUpdate()
     {
         // Update physics in this callback to avoid jittering!
+
         // Applying forces to the plane
         rb.AddForce(maxThrust * throttle * transform.forward);
         // Torque is rotational force
@@ -53,22 +78,31 @@ public class PlaneHandler : MonoBehaviour
         // Adding lift when the plane is horizontal relative to the ground
         rb.AddForce(rb.velocity.magnitude * liftForce * transform.up);
     }
+    //private void HandleInput()    // Legacy input system
+    //{
+    //    // Setting rotational values from axis inputs
+    //    roll = Input.GetAxis("Roll");
+    //    pitch = Input.GetAxis("Pitch");
+    //    yaw = Input.GetAxis("Yaw");
+    //    //Debug.Log(yaw);
+    //    // Handling throttle inputs
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        throttle += throttleIncrement;
+    //    }
+    //    else if (Input.GetKey(KeyCode.S))
+    //    {
+    //        throttle -= throttleIncrement;
+    //    }
+    //    throttle = Mathf.Clamp(throttle, 0f, 100f);
+    //    Debug.Log("Current throttle: " + throttle);
+    //}
+
     private void HandleInput()
     {
-        // Setting rotational values from axis inputs
-        roll = Input.GetAxis("Roll");
-        pitch = Input.GetAxis("Pitch");
-        yaw = Input.GetAxis("Yaw");
-
-        // Handling throttle inputs
-        if(Input.GetKey(KeyCode.W))
-        {
-            throttle += throttleIncrement;
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            throttle -= throttleIncrement;
-        }
+        roll = stickValue.x;
+        pitch = stickValue.y;
+        throttle += (throttleIncrement * throttleValueTrigger);
         throttle = Mathf.Clamp(throttle, 0f, 100f);
     }
 }
