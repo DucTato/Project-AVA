d6c6939015d06fff4da29030c54a3ee60f52fbc5
DucTato <65166409+DucTato@users.noreply.github.com>
2024-10-07 20:47:20 +0700

    Project cleaned
    Removed non-working packages

--- Assets/voxul/Scripts/Utilities/FontExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-namespace Voxul.Utilities
-{
-	public static class FontExtensions
-	{
-		public static char GetCharacter(this CharacterInfo info) => Convert.ToChar(info.index);
-	}
-}
