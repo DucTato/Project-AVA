c5c9d6c4e32895620750434fcfa6e98b375b65f9
DucTato <65166409+DucTato@users.noreply.github.com>
2024-10-30 15:24:10 +0700

    f15 eagle
    Added f15 - eagle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,37 @@ public class PlayerController : MonoBehaviour
 {
     // Adding a PlayerController Singleton
     public static PlayerController instance;
+    [SerializeField]
+    private PlaneHandler planeHandler;
+    [SerializeField]
+    private HeliHandler heliHandler;
     private GamepadControls gpControls;
+    [SerializeField]
+    private Vector3 controlInput;
     private Vector2 stickValue;
     private void Awake()
     {
         instance = this;
         gpControls = new GamepadControls();
-        gpControls.Gameplay.ACmovement.performed += context => stickValue = context.ReadValue<Vector2>();
-        gpControls.Gameplay.ACmovement.canceled += context => stickValue = Vector2.zero;
+        gpControls.Gameplay.ACmovement.performed += context => { 
+            stickValue = context.ReadValue<Vector2>();
+            //controlInput = new Vector3(stickValue.y, controlInput.y, -stickValue.x);
+            controlInput.x = stickValue.y;
+            controlInput.z = -stickValue.x;
+        };
+        gpControls.Gameplay.ACmovement.canceled += context => { stickValue = Vector2.zero;
+            controlInput = Vector3.zero;
+        };
+
+        gpControls.Gameplay.ACyaw.performed += context => {
+            //controlInput = new Vector3(controlInput.x, context.ReadValue<float>(), controlInput.z);
+            controlInput.y = context.ReadValue<float>();
+        };
+        gpControls.Gameplay.ACyaw.canceled += context => { 
+            //controlInput = new Vector3(controlInput.x, 0, controlInput.z);
+            controlInput.y = 0;
+        };
+        gpControls.Gameplay.ACthrottle.performed += context => { if (planeHandler != null) planeHandler.SetThrottleInput(context.ReadValue<float>()); };
     }
     // Start is called before the first frame update
     void Start()
@@ -24,11 +47,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInputs();
     }
     private void HandleInputs()
     {
-
+        if (planeHandler != null)
+        {
+            planeHandler.SetControlInput(controlInput);
+        }
     }
     private void OnEnable()
     {
