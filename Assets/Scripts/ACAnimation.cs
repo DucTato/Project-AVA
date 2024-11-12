5de83030ab5bdaa6c76e223f0c840b256bcfe505
DucTato <65166409+DucTato@users.noreply.github.com>
2024-11-12 22:25:37 +0700

    Updated engine animation

--- Assets/Scripts/ACAnimation.cs
+++ Assets/Scripts/ACAnimation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ACAnimation : MonoBehaviour
 {
@@ -10,10 +11,13 @@ public class ACAnimation : MonoBehaviour
     private Light[] engineLight;
     [SerializeField]
     private float abMaxVal, engLightMaxVal;
+    [SerializeField]
+    private VisualEffect heatHaze;
     private void Awake()
     {
         engineLight = GetComponentsInChildren<Light>();
         afterBurners = GetComponentsInChildren<ParticleSystem>();
+        heatHaze = GetComponentInChildren<VisualEffect>();
         abMaxVal = afterBurners[0].startLifetime;
         engLightMaxVal = engineLight[0].intensity;
     }
@@ -30,6 +34,8 @@ public class ACAnimation : MonoBehaviour
     }
     public void SetEnginePowerVisual(float powValue)
     {
+        if (powValue >= 0.8f) heatHaze.enabled = true; 
+        else heatHaze.enabled = false;
         // power value is the % of engine's max power
         Mathf.Clamp(powValue, 0.0f, 1.0f);
         foreach (Light light in engineLight) light.intensity = powValue * engLightMaxVal;
